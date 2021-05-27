5ba1ac20a3c067c07111dfea671ef0d9f1362213
Pato Bonilla <gpbonillas@gmail.com>
2021-05-27 15:59:52 -0500

    Create a form with GlobalKey

---
+++ lib/my_custom_form.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          // Add TextFormFields and ElevatedButton here.
+        ],
+      ),
+    );
+  }
+}
